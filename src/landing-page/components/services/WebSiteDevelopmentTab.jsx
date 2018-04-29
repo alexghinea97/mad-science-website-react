300af29876b8a0953bc8ef46adc9bd9c654d1479
Bojo <stil.2910@gmail.com>
2018-04-29 17:03:36 +0300

    adding a template for the services

--- src/landing-page/components/services/WebSiteDevelopmentTab.jsx
+++ src/landing-page/components/services/WebSiteDevelopmentTab.jsx
@@ -11,6 +11,8 @@ import {
 import Dialog from 'material-ui/Dialog';
 import FlatButton from 'material-ui/FlatButton';
 import { RaisedButton } from 'material-ui';
+import { Step, Stepper, StepLabel } from 'material-ui/Stepper';
+import { List, ListItem } from 'material-ui/List';
 
 export default class WebSiteDevelopmentTab extends Component {
   constructor(props) {
@@ -46,6 +48,30 @@ export default class WebSiteDevelopmentTab extends Component {
       <div>
         <h3>WebSite Developent</h3>
         <p>This is an example tab.</p>
+        <Stepper activeStep={0}>
+          <Step>
+            <StepLabel>Select campaign settings</StepLabel>
+          </Step>
+          <Step>
+            <StepLabel>Create an ad group</StepLabel>
+          </Step>
+          <Step>
+            <StepLabel>Create an ad</StepLabel>
+          </Step>
+        </Stepper>
+
+        <h4>Templates</h4>
+        <List>
+          <ListItem>
+            <a target="blank" href="/templates/band-template.html">
+              Band Template
+            </a>
+          </ListItem>
+          <ListItem primaryText="Trash" />
+          <ListItem primaryText="Spam" />
+          <ListItem primaryText="Follow up" />
+        </List>
+
         <Table>
           <TableHeader>
             <TableRow>
@@ -56,36 +82,62 @@ export default class WebSiteDevelopmentTab extends Component {
           <TableBody>
             <TableRow>
               <TableRowColumn>
-                <RaisedButton label="John Smith" onClick={this.handleOpen} />{' '}
+                <RaisedButton
+                  fullWidth={true}
+                  label="Basic static website"
+                  onClick={this.handleOpen}
+                />
               </TableRowColumn>
               <TableRowColumn>Employed</TableRowColumn>
             </TableRow>
             <TableRow>
-              <TableRowColumn>Randal White</TableRowColumn>
+              <TableRowColumn>
+                <RaisedButton
+                  fullWidth={true}
+                  label="Google analytics integration"
+                  onClick={this.handleOpen}
+                />
+              </TableRowColumn>
               <TableRowColumn>Unemployed</TableRowColumn>
             </TableRow>
             <TableRow>
-              <TableRowColumn>Stephanie Sanders</TableRowColumn>
+              <TableRowColumn>
+                <RaisedButton
+                  fullWidth={true}
+                  label="Facebook pixel integration"
+                  onClick={this.handleOpen}
+                />
+              </TableRowColumn>
               <TableRowColumn>Employed</TableRowColumn>
             </TableRow>
             <TableRow>
-              <TableRowColumn>Steve Brown</TableRowColumn>
+              <TableRowColumn>
+                <RaisedButton
+                  fullWidth={true}
+                  label="Localization"
+                  onClick={this.handleOpen}
+                />
+              </TableRowColumn>
               <TableRowColumn>Employed</TableRowColumn>
             </TableRow>
             <TableRow>
-              <TableRowColumn>Christopher Nolan</TableRowColumn>
+              <TableRowColumn>
+                <RaisedButton
+                  fullWidth={true}
+                  label="Users and Authentication"
+                  onClick={this.handleOpen}
+                />
+              </TableRowColumn>
               <TableRowColumn>Unemployed</TableRowColumn>
             </TableRow>
           </TableBody>
         </Table>
-        <Dialog
-          title="Dialog With Actions"
-          actions={actions}
+        <Dialog>
+          title="Dialog With Actions" actions={actions}
           modal={false}
           open={this.state.open}
           onRequestClose={this.handleClose}
-        >
-          The actions in this window were passed in as an array of React
+          > The actions in this window were passed in as an array of React
           objects.
         </Dialog>
       </div>
