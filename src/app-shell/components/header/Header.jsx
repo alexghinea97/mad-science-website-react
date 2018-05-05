74187297667aa39abd494a114d94fbb942c6e4b5
didi <didimilikina8@gmail.com>
2018-05-05 18:20:38 +0300

    Added two langueged text - en, bg

--- src/app-shell/components/header/Header.jsx
+++ src/app-shell/components/header/Header.jsx
@@ -2,9 +2,11 @@ import React, { Component } from 'react';
 import { NavLink } from 'react-router-dom';
 import './Header.css';
 import CommonNavigation from '../navigation/CommonNavigation';
+import strings from '../../../LocalizedStrings';
 
 export default class Header extends Component {
   render() {
+    const headerStrings = strings.header;
     return (
       <header
         id="page-header"
