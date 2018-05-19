03d4ad105b35b48f268049fdabcb3bda8f5e4df0
didi <didimilikina8@gmail.com>
2018-05-19 23:06:24 +0300

    Refactored Website service modal.

--- src/landing-page/components/services/components/services-tabs/WebsiteServiceModal.jsx
+++ src/landing-page/components/services/components/services-tabs/WebsiteServiceModal.jsx
@@ -1,26 +1,37 @@
 import React from 'react';
 import Button from '@material-ui/core/Button';
-import Dialog from 'material-ui/Dialog';
+import Dialog from '@material-ui/core/Dialog';
+import DialogTitle from '@material-ui/core/DialogTitle';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogContentText from '@material-ui/core/DialogContentText';
+import DialogActions from '@material-ui/core/DialogActions';
 import LocalizedStrings from '../../../../../LocalizedStrings';
 
 export default function websiteServiceModal(props) {
   const actions = [
-    <Button color="primary" onClick={props.handleClose}>
+    <Button variant="raised" color="primary" onClick={props.handleClose}>
       {LocalizedStrings.buttons.cancel}
     </Button>
   ];
+  // fullWidth
+  //     actions={actions}
+  //     modal={false}
+  //     open={props.open}
+  //     onRequestClose={props.handleClose}
+  //     autoScrollBodyContent={true}
+  //     autoDetectWindowHeight={true}
 
   return (
-    <Dialog
-      title={props.title}
-      actions={actions}
-      modal={false}
-      open={props.open}
-      onRequestClose={props.handleClose}
-      autoScrollBodyContent={true}
-      autoDetectWindowHeight={true}
-    >
-      {props.children}
+    <Dialog open={props.open} fullWidth>
+      <DialogTitle>{props.title}</DialogTitle>
+      <DialogContent>
+        <DialogContentText>{props.children}</DialogContentText>
+      </DialogContent>
+      <DialogActions>
+        <Button onClick={props.handleClose} color="primary">
+          {actions}
+        </Button>
+      </DialogActions>
     </Dialog>
   );
 }
