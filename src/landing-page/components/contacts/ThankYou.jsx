9bcb552bf81ba5c1d7ede30f0c9505fd9b1ee5bb
didi <didimilikina8@gmail.com>
2018-05-19 21:46:26 +0300

    Refactored thank you form.

--- src/landing-page/components/contacts/ThankYou.jsx
+++ src/landing-page/components/contacts/ThankYou.jsx
@@ -1,28 +1,36 @@
 import React, { Component } from 'react';
-import Dialog from 'material-ui/Dialog';
-import RaisedButton from 'material-ui/RaisedButton';
+import Dialog from '@material-ui/core/Dialog';
+import DialogTitle from '@material-ui/core/DialogTitle';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogContentText from '@material-ui/core/DialogContentText';
+import Button from '@material-ui/core/Button';
 import LocalizedStrings from '../../../LocalizedStrings';
 
 export default class ThankYou extends Component {
   render() {
     const actions = [
-      <RaisedButton
-        label={LocalizedStrings.body.contacts.thankYou.closeButton}
-        primary={true}
-        onClick={this.props.handleClose}
-      />
+      <Button color="primary" variant="raised" onClick={this.props.handleClose}>
+        {LocalizedStrings.body.contacts.thankYou.closeButton}
+      </Button>
     ];
 
     return (
       <div>
-        <Dialog
-          title={LocalizedStrings.body.contacts.thankYou.title}
-          actions={actions}
-          modal={false}
-          open={this.props.open}
-          onRequestClose={this.handleClose}
-        >
-          {LocalizedStrings.body.contacts.thankYou.description}
+        <Dialog open={this.props.open} onClose={this.handleClose}>
+          <DialogTitle>
+            {LocalizedStrings.body.contacts.thankYou.title}
+          </DialogTitle>
+          <DialogContent>
+            <DialogContentText>
+              {LocalizedStrings.body.contacts.thankYou.description}
+            </DialogContentText>
+          </DialogContent>
+          <DialogActions>
+            <Button onClick={this.props.handleClose} color="primary">
+              {actions}
+            </Button>
+          </DialogActions>
         </Dialog>
       </div>
     );
