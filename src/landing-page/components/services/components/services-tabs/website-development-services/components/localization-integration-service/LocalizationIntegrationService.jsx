348c54b6d8cb53e8b9e3c92c82034fe170c793bd
didi <didimilikina8@gmail.com>
2018-05-20 14:58:27 +0300

    Refactored Localization integration service.

--- src/landing-page/components/services/components/services-tabs/website-development-services/components/localization-integration-service/LocalizationIntegrationService.jsx
+++ src/landing-page/components/services/components/services-tabs/website-development-services/components/localization-integration-service/LocalizationIntegrationService.jsx
@@ -1,46 +1,130 @@
 import React from 'react';
 import Select from '@material-ui/core/Select';
-import MenuItem from 'material-ui/MenuItem';
+import Menu from '@material-ui/core/Menu';
+import MenuItem from '@material-ui/core/MenuItem';
 import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
+import ListItemText from '@material-ui/core/ListItemText';
+import FormControl from '@material-ui/core/FormControl';
+import InputLabel from '@material-ui/core/InputLabel';
+import Chip from '@material-ui/core/Chip';
+import Input from '@material-ui/core/Input';
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
 import ActionDelete from 'material-ui/svg-icons/action/delete';
 import LocalizedStrings from '../../../../../../../../LocalizedStrings';
 import Button from '@material-ui/core/Button';
 
-export default class LocalizationIntegrationService extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      value: 0,
-      selectedLanguages: new Set()
-    };
+const styles = theme => ({
+  root: {
+    display: 'flex',
+    flexWrap: 'wrap',
+    width: '-moz-available'
+  },
+  formControl: {
+    margin: theme.spacing.unit,
+    minWidth: '50%'
+  },
+  chips: {
+    display: 'flex',
+    flexWrap: 'wrap'
+  },
+  chip: {
+    margin: theme.spacing.unit / 4
   }
+});
 
-  handleChange = (event, index, value) => {
-    this.state.selectedLanguages.add(event.target.innerText);
-    this.setState({ value: value });
+const MenuProps = {
+  PaperProps: {
+    style: {
+      maxHeight: 48 * 4.5 + 8,
+      width: 250
+    }
+  }
+};
+
+class LocalizationIntegrationService extends React.Component {
+  // constructor(props) {
+  //   super(props);
+  //   this.state = {
+  //     value: 0,
+  //     selectedLanguages: new Set()
+  //   };
+  // }
+
+  state = {
+    name: []
   };
 
+  handleChange = event => {
+    this.setState({ name: event.target.value });
+  };
+
+  // handleChange = (event, index, value) => {
+  //   this.state.selectedLanguages.add(event.target.innerText);
+  //   this.setState({ value: value });
+  // };
+
   render() {
+    const { classes, theme } = this.props;
+
     const languages = [
       'Английски',
       'Български',
       'Испански',
       'Френски',
-      'Немски'
+      'Немски',
+      'Японски',
+      'Китайски'
     ];
-    const items = [];
 
-    for (let i = 0; i < languages.length; i++) {
-      items.push(<MenuItem value={i} primaryText={languages[i]} />);
-    }
+    // const items = [];
+
+    // for (let i = 0; i < languages.length; i++) {
+    //   items.push(<MenuItem value={i} primaryText={languages[i]} />);
+    // }
 
     return (
-      <div>
+      <div className={classes.root}>
         <p>{LocalizedStrings.body.services.localizationDialog.textOne}</p>
         <p>{LocalizedStrings.body.services.localizationDialog.textSecond}</p>
         <p>{LocalizedStrings.body.services.localizationDialog.textThird}</p>
-        <Select
+
+        <div className={classes.root}>
+          <FormControl className={classes.formControl}>
+            <InputLabel htmlFor="select-multiple-chip">Languages</InputLabel>
+            <Select
+              multiple
+              value={this.state.name}
+              onChange={this.handleChange}
+              input={<Input id="select-multiple-chip" />}
+              renderValue={selected => (
+                <div className={classes.chips}>
+                  {selected.map(value => (
+                    <Chip key={value} label={value} className={classes.chip} />
+                  ))}
+                </div>
+              )}
+              MenuProps={MenuProps}
+            >
+              {languages.map(name => (
+                <MenuItem
+                  key={name}
+                  value={name}
+                  style={{
+                    fontWeight:
+                      this.state.name.indexOf(name) === -1
+                        ? theme.typography.fontWeightRegular
+                        : theme.typography.fontWeightMedium
+                  }}
+                >
+                  {name}
+                </MenuItem>
+              ))}
+            </Select>
+          </FormControl>
+        </div>
+        {/* <Select
           maxHeight={120}
           value={this.state.value}
           onChange={this.handleChange}
@@ -53,7 +137,7 @@ export default class LocalizationIntegrationService extends React.Component {
               <ListItem primaryText={language} rightIcon={<ActionDelete />} />
             );
           })}
-        </List>
+        </List> */}
         <Button
           fullWidth
           variant="raised"
@@ -66,3 +150,11 @@ export default class LocalizationIntegrationService extends React.Component {
     );
   }
 }
+
+LocalizationIntegrationService.propTypes = {
+  classes: PropTypes.object.isRequired,
+  theme: PropTypes.object.isRequired
+};
+export default withStyles(styles, { withTheme: true })(
+  LocalizationIntegrationService
+);
