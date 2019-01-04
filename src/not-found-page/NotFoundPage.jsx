d8350ee0568cce637ea6cb5110aa90a8fdb91a75
Didi Milikina <didimilikina8@gmail.com>
2019-01-04 21:50:40 +0200

    Added new page for 404 and image with cat

--- src/not-found-page/NotFoundPage.jsx
+++ src/not-found-page/NotFoundPage.jsx
@@ -1,13 +1,38 @@
 import React, { Component } from 'react';
+import Button from '@material-ui/core/Button';
+import './NotFound.css';
 
 export default class NotFoundPage extends Component {
   render() {
     return (
       <div
+        style={{ backgroundColor: '#FCFCFC' }}
         id="not-found-page"
         className="demo-layout-transparent mdl-layout mdl-js-layout"
       >
-        <main className="mdl-layout__content" />
+        <img
+          src="/images/not-found.jpg"
+          style={{
+            width: '50%',
+            height: '60%',
+            display: 'block',
+            margin: 'auto',
+            position: 'fixed',
+            top: '40%'
+          }}
+        />
+        {/* <main className="mdl-layout__content" /> */}
+        <h2>Oops ...this page is not found</h2>
+
+        <Button
+          fullWidth
+          variant="raised"
+          color="primary"
+          onClick={this.handleAddService}
+          style={{ marginLeft: '35%', marginTop: '40px', width: '30%' }}
+        >
+          {'Go back'}
+        </Button>
       </div>
     );
   }
